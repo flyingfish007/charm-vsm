abd3f9b19a8f92d45ea8678da5dd8db4ab603dcb
flyingfish007 <bxzhu_5355@163.com>
2016-06-22 15:33:00 +0800

    fix the issue of vsm_hooks.py

--- vsm/hooks/vsm_hooks.py
+++ vsm/hooks/vsm_hooks.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import sys
 import utils
 from subprocess import check_call
 from charmhelpers.core.hookenv import (
@@ -8,7 +9,8 @@ from charmhelpers.core.hookenv import (
     network_get_primary_address,
     unit_get,
     relation_set,
-    relation_get
+    relation_get,
+    UnregisteredHookError
 )
 from charmhelpers.contrib.openstack.utils import sync_db_with_multi_ipv6_addresses
 
@@ -61,3 +63,10 @@ def identity_joined():
 @hooks.hook('identity-service-relation-changed')
 def identity_changed():
     utils.juju_log('info', '**********identity-service-relation-changed')
+
+
+if __name__ == '__main__':
+    try:
+        hooks.execute(sys.argv)
+    except UnregisteredHookError as e:
+        utils.juju_log('warn', 'Unknown hook {} - skipping.'.format(e))
