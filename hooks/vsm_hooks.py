dc7a13a4c01b46000e0dedb6a2d941427c3ad70b
flyingfish007 <bxzhu_5355@163.com>
2016-06-24 13:51:06 +0800

    initial charm-vsm

--- hooks/vsm_hooks.py
+++ hooks/vsm_hooks.py
@@ -13,45 +13,58 @@ from charmhelpers.core.hookenv import (
     UnregisteredHookError
 )
 from charmhelpers.contrib.openstack.utils import sync_db_with_multi_ipv6_addresses
+from charmhelpers.contrib.openstack import context
 
 hooks = Hooks()
 
 
-@hooks.hook('shared-db-relation-joined')
-def db_joined():
-    utils.juju_log('info', '**********shared-db-relation-joined')
-    if config('prefer-ipv6'):
-        sync_db_with_multi_ipv6_addresses(config('database'),
-                                          config('database-user'))
-    else:
-        try:
-            # NOTE: try to use network spaces
-            host = network_get_primary_address('shared-db')
-        except NotImplementedError:
-            # NOTE: fallback to private-address
-            host = unit_get('private-address')
-        conf = config()
-        relation_set(database=conf['database'],
-                     username=conf['database-user'],
-                     hostname=host)
-
-
-@hooks.hook('shared-db-relation-changed')
-def db_changed():
-    utils.juju_log('info', '**********shared-db-relation-changed')
-
-    # # to initial the vsm db
-    # cmd = ['vsm-manage', 'db', 'sync']
-    # check_call(cmd)
+# @hooks.hook('shared-db-relation-joined')
+# def db_joined():
+#     utils.juju_log('info', '**********shared-db-relation-joined')
+#     if config('prefer-ipv6'):
+#         sync_db_with_multi_ipv6_addresses(config('database'),
+#                                           config('database-user'))
+#     else:
+#         try:
+#             # NOTE: try to use network spaces
+#             host = network_get_primary_address('shared-db')
+#         except NotImplementedError:
+#             # NOTE: fallback to private-address
+#             host = unit_get('private-address')
+#         conf = config()
+#         relation_set(database=conf['database'],
+#                      username=conf['database-user'],
+#                      hostname=host)
+#
+#
+# @hooks.hook('db-relation-changed')
+# def db_changed():
+#     utils.juju_log('info', '**********db-relation-changed')
+#
+#     # # to initial the vsm db
+#     # cmd = ['vsm-manage', 'db', 'sync']
+#     # check_call(cmd)
 
 
 @hooks.hook('amqp-relation-joined')
-def amqp_joined():
+def amqp_joined(relation_id=None):
     utils.juju_log('info', '**********amqp-relation-joined')
-
+    conf = config()
+    relation_set(relation_id=relation_id,
+                 username=conf['rabbit-user'], vhost=conf['rabbit-vhost'])
 
 @hooks.hook('amqp-relation-changed')
 def amqp_changed():
+    ctx = context.AMQPContext().__call__()
+    rabbitmq_host = ctx['rabbitmq_host']
+    rabbitmq_user = ctx['rabbitmq_user']
+    rabbitmq_password = ctx['rabbitmq_password']
+    rabbitmq_virtual_host = ctx['rabbitmq_virtual_host']
+
+    utils.juju_log('info', '**********rabbitmq_host is %s' % rabbitmq_host)
+    utils.juju_log('info', '**********rabbitmq_user is %s' % rabbitmq_user)
+    utils.juju_log('info', '**********rabbitmq_password is %s' % rabbitmq_password)
+    utils.juju_log('info', '**********rabbitmq_virtual_host is %s' % rabbitmq_virtual_host)
     utils.juju_log('info', '**********amqp-relation-changed')
 
 
